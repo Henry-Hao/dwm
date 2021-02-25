4790d0fa4ce50634c4125345a4b6390586f0ac21
Henry Hao <haoyixian1994@hotmail.com>
2021-02-25 00:03:56 -0500

    1. Added basic keybindings and configs.

--- config.h
+++ config.h
@@ -1,7 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
+#define TERMINAL         "alacritty"
+#define TERMCLASS        "Alacritty"
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -10,45 +12,60 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "UbuntuMono Nerd:size=10","NotoEmoji Nerd:pixle=10" };
+static const char dmenufont[]       = "UbuntuMono Nerd:size=10";
+static const char normbgcolor[]     = "#222222";
+static const char normbordercolor[] = "#444444";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selfgcolor[]      = "#eeeeee";
+static const char selbordercolor[]  = "#770000";
+static const char selbgcolor[]      = "#005577";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg           bg           border   */
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor},
 };
 
 typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"alacritty", "--title", "vimwiki", "-e", "nvim", "-c", "VimwikiIndex", NULL };
+const char *spcmd1[] = {TERMINAL, "--title", "vimwiki", "-e", "nvim", "-c", "VimwikiIndex", NULL };
 const char *spcmd2[] = {"keepassxc", NULL };
+const char *spcmd3[] = {TERMINAL, "--title", "float", "-e", "calc", "-f", "mono:pixelsize=24", NULL };
+const char *spcmd4[] = {TERMINAL, "--title", "float", "-e", "htop",  NULL };
+const char *spcmd5[] = {TERMINAL, "--title", "float", "-e", "nmtui",  NULL };
+const char *spcmd6[] = {TERMINAL, "--title", "float", "-e", "pulsemixer",  NULL };
+const char *spcmd7[] = {TERMINAL, "--title", "float", "-e", "calcurse",  NULL };
+const char *spcmd8[] = {TERMINAL, "--title", "float", "-e", "pulsemixer", "--toogle-mute", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"vimwiki",       spcmd1},
-	{"spranger",      spcmd2},
+	{"keepassxc",     spcmd2},
+	{"calculator",    spcmd3},
+	{"htop",          spcmd4},
+	{"nmtui",         spcmd5},
+	{"pulsemixer",    spcmd6},
+	{"calcurse",      spcmd7},
+	{"pulsemixer",    spcmd8},
 };
 
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1: ", "2: ", "3: ", "4", "5", "6", "7", "8", "9: " };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance      title       tags mask     isfloating  monitor */
-	{ "Gimp",     NULL,         NULL,       0,            1,          -1 },
-	{ "Firefox",  NULL,         NULL,       1 << 8,       0,          -1 },
+	{ NULL,       NULL,         "float",    0,            1,          -1 },
+	{ NULL,       NULL,         "main",     1,            0,          -1 },
+	{ "firefox",  NULL,         NULL,       1 << 1,       0,          -1 },
+	{ "Pcmanfm",  NULL,         NULL,       1 << 2,       1,          -1 },
+	{ "Skype",    NULL,         NULL,       1 << 8,       0,          -1 },
 	{ NULL,       NULL,         "vimwiki",  SPTAG(0),     1,          -1 },
-	{ NULL,       "spfm",       NULL,       SPTAG(1),     1,          -1 },
-	{ NULL,       "keepassxc",  NULL,       SPTAG(2),     0,          -1 },
+	{ NULL,       "keepassxc",  NULL,       SPTAG(1),     1,          -1 },
 
 };
 
@@ -92,18 +109,42 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          SHCMD("firefox") },
+	{ NULL,                         XK_Print,  spawn,          SHCMD("flameshot gui") },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD(TERMINAL " --title float -e ranger") },
+	{ MODKEY,                       XK_c,      togglescratch,  {.ui = 2 } }, // scratchpad for calculator
+	{ MODKEY|ShiftMask,             XK_i,      togglescratch,  {.ui = 3 } }, // scratchpad for htop
+	{ MODKEY|ShiftMask,             XK_w,      togglescratch,  {.ui = 4 } }, // scratchpad for nmtui
+	{ MODKEY|ShiftMask,             XK_v,      togglescratch,  {.ui = 5 } }, // scratchpad for pulsemixer
+	{ MODKEY|ShiftMask,             XK_p,      togglescratch,  {.ui = 6 } }, // scratchpad for calcurse
+	{ MODKEY|ShiftMask,             XK_m,      togglescratch,  {.ui = 7 } }, // scratchpad for pulsemixer-mute
+	{ MODKEY|ShiftMask,             XK_c,      spawn,          SHCMD(TERMINAL " --title float -e camtoggle &") },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          SHCMD("pcmanfm &") },
+	{ MODKEY,                       XK_r,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	// { MODKEY,                       XK_F1,      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F2,      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F3,      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F4,      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F5,      spawn,         {.i = -1 } },
+	{ MODKEY,                       XK_F6,     spawn,          SHCMD(TERMINAL " --title float -e qbittorrent") },
+	// { MODKEY,                       XK_F7,      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F8,      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F9,      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F10      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F11      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_F12      spawn,         {.i = -1 } },
+	// { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	// { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ NULL,                         XK_F9,     view,           {.ui = 1 << 8} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
@@ -124,7 +165,7 @@ static Key keys[] = {
 	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -137,8 +178,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_p,      togglescratch,  {.ui = 0 } }, // scratchpad for vimwiki
-  { MODKEY,                       XK_x,      togglescratch,  {.ui = 1 } },
-
+	{ MODKEY,                       XK_x,      togglescratch,  {.ui = 1 } }, // scratchpad for keepassxc
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
